b27208693c831bdac81ee2e980effa9a4309d258
vhuhwavho <vvhuhwavho8@gmail.com>
2025-11-21 04:56:18 +0200

    mobile app connect

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,7 @@ const isPublicRoute = createRouteMatcher([
   "/api/leaves",
   "/api/subtasksMobile/(.*)",
   "/api/time-entries-mobile/(.*)",
+  "/api/documents",
 ]);
 
 const allowedOrigins = [
